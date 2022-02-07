0b5cd3af18a194057e4c6455f489d120a231f238
40231913 <plogan04@qub.ac.uk>
2022-02-07 17:56:36 +0000

    Inital set up

---
+++ Mini-lab Java Refresher 1-Simple banking app/src/tests/UserTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import model.User;
+
+public class UserTest {
+	
+	public static void main(String[] args) {
+		User testUser = new User("mike", "my_password", "Mike", "Smith", "07771234567");
+	
+		System.out.println(testUser.toString());
+	}
+}
